21cca51407ff4f61bae84283a34488051cd792b4
RCMAN <franco.gaetan@gmail.com>
2025-02-05 07:02:35 -0500

    Add files via upload

---
+++ sdl3/sdl3_cmake/build/CMakeFiles/newstart.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/newstart.dir/Examples/menu.cpp.o"
+  "CMakeFiles/newstart.dir/Examples/menu.cpp.o.d"
+  "newstart"
+  "newstart.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/newstart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
